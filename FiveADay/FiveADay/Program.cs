7f5c6b2e5175579142e8745883b581bce972013f
AmjedAgabani <amjedagabani@gmail.com>
2017-03-21 10:03:48 +0000

    Initial commit

---
+++ FiveADay/FiveADay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveADay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Okay " + name + ", How many fruits have you had to eat today?");
+            int numberOfFruit = int.Parse(Console.ReadLine());
+
+            if (numberOfFruit > 4)
+            {
+                Console.WriteLine("Good job, " + name + ". You've had your five a day!");
+            }
+            else
+                Console.WriteLine("What are you doing " + name + "?!? Go and have your five a day!");
+        }
+    }
+}
